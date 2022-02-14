4f5759f53bffa78250a5c019a5d804bbf9c5c508
Reza Aghajani <rezaaghajani@protonmail.com>
2022-02-14 21:57:48 +0330

    Create component for Music Player

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  // create music player component
+  <Music />
+
+</template>
+
+<script>
+import Music from './components/music-player.vue'
+
+
+export default {
+  name: 'App',
+  setup() {
+    return {
+      message: 'Hello Vue!'
+    }
+  },
+  components: {
+    Music
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
